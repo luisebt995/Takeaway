f052c9eb0c4f4087a658c477f16462de7800034f
Luis Bustamante <luisebt995@gmail.com>
2024-04-02 16:40:25 +0200

    Design the menu of the app, using List, HStack, ZStack, Text and Image.

--- Takeaway/ContentView.swift
+++ Takeaway/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                //Text("Comida 1")
+                VistaTipoComida(imageName: "asiatica", foodName: "Asiatica")
+                VistaTipoComida(imageName: "arabe", foodName: "Arabe")
+                VistaTipoComida(imageName: "indu", foodName: "Indu")
+                VistaTipoComida(imageName: "mediterranea", foodName: "Mediterranea")
+            }.navigationTitle("Haga su pedido: ")
+        }
+    }
+}
+
+struct VistaTipoComida : View {
+    
+    var imageName : String
+    var foodName : String
+    
+    var body: some View{
+        HStack{
+            Spacer()
+            ZStack{
+                Image(imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 250, height: 100)
+                    .clipped()
+                    .cornerRadius(20.0)
+            
+                Text(foodName)
+                    .font(.custom("Helvetica-Medium",size: 34))
+                    .bold()
+                    .foregroundColor(.white)
+            }
+            Spacer()
+        }
+        .padding(.top,5)
+        .padding(.bottom,5)
     }
 }
 
