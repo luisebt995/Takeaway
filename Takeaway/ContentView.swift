11f2d8d048dc5ce68dd6428a74174e7db79ae5db
Luis Bustamante <luisebt995@gmail.com>
2024-04-02 15:22:20 +0200

    Initial Commit

---
+++ Takeaway/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Takeaway
+//
+//  Created by Luis on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Takeaway/TakeawayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeawayApp.swift
+//  Takeaway
+//
+//  Created by Luis on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TakeawayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
